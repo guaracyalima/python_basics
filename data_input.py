75232cff974a69433ce70ddbbc3d4c137a51f77b
guaracyalima <guaracyaraujolima@gmail.com>
2017-11-17 11:20:40 -0200

    files and loops

---
+++ data_input.py
@@ -0,0 +1,8 @@
+def media(a, b):
+    r = (a + b) / 2
+
+    return r
+
+k = media(6, 9)
+
+print(k)
---
+++ loop.py
@@ -0,0 +1,10 @@
+cont = 0
+
+while cont <= 10:
+    print ('Ro tuma cana')
+    cont = cont + 1
+
+#for
+#o 2 e a quantidade de vezes qo o codigo sera repetido
+for _ in range(1, 11, 2):
+    print('sucu de manga')
